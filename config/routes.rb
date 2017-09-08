3e28544e42de15df5adad12e7a238044750151be
Matt Loria <loria.matt@gmail.com>
2017-09-08 00:17:43 -0400

    Clear ruby home page. Add support email address for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+	root to: 'home#show'
+
   mount_devise_token_auth_for 'User', at: 'auth'
 
   scope module: 'api' do
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
 	    resources :users, only: [:index, :show]
 	  end
 	end
-	
+
 end
